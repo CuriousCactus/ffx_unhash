ce3e871dc1bba8f2f18f11c589d13561f46ab0de
Oliver Gould <oliver.gould@nottingham.ac.uk>
2025-08-31 10:05:01 +0100

    Added checkpoints

--- utils/hash_utils.py
+++ utils/hash_utils.py
@@ -37,44 +37,60 @@ def search_for_known_hashes(
     start = time.time()
 
     print("CPU count:", cpu_count())
-
     pool = Pool(cpu_count())
-    hash_hits_iterator = pool.imap_unordered(
-        partial(check_hash, known_track_hashes),
-        generate_potential_track_names(
-            sec1_list,
-            sep1_list,
-            sec2_list,
-            sep2_list,
-            sec3_list,
-            sep3_list,
-            sec4_list,
-            sep4_list,
-            sec5_list,
-        ),
-    )
-
-    hash_hits = [x for x in hash_hits_iterator if x[0] is not None]
 
     re_found_hits = []
-    new_hists = []
-    for hash_hit, potential_track_name in hash_hits:
-        if hash_hit is None:
-            continue
+    new_hits = []
 
-        elif potential_track_name in known_track_names:
-            print(f"Known hit: {potential_track_name} -> {hash_hit}")
-            re_found_hits.append(potential_track_name)
+    checkpoint_size = max(1, len(sec1_list) // 100)
 
-        elif hash_hit is not None:
-            print(f"New hit: {potential_track_name} -> {hash_hit}")
-            new_hists.append(potential_track_name)
-            write_track_name(potential_track_name, hash_hit, output_file_name)
+    checkpoint_number = len(sec1_list) // checkpoint_size
+    if len(sec1_list) % checkpoint_size != 0:
+        checkpoint_number += 1
+
+    checkpoint_start = 0
+
+    while checkpoint_start < len(sec1_list):
+        print("----------------------------------------------------")
+        print(f"Checkpoint number: {checkpoint_start // checkpoint_size}/{checkpoint_number}")
+
+        checkpoint_max = min(checkpoint_start + checkpoint_size, len(sec1_list))
+        hash_hits_iterator = pool.imap_unordered(
+            partial(check_hash, known_track_hashes),
+            generate_potential_track_names(
+                sec1_list[checkpoint_start:checkpoint_max],
+                sep1_list,
+                sec2_list,
+                sep2_list,
+                sec3_list,
+                sep3_list,
+                sec4_list,
+                sep4_list,
+                sec5_list,
+            ),
+        )
+
+        hash_hits = [x for x in hash_hits_iterator if x[0] is not None]
+
+        for hash_hit, potential_track_name in hash_hits:
+            if hash_hit is None:
+                continue
+
+            elif potential_track_name in known_track_names:
+                print(f"Known hit: {potential_track_name} -> {hash_hit}")
+                re_found_hits.append(potential_track_name)
+
+            elif hash_hit is not None:
+                print(f"New hit: {potential_track_name} -> {hash_hit}")
+                new_hits.append(potential_track_name)
+                write_track_name(potential_track_name, hash_hit, output_file_name)
+
+        checkpoint_start += checkpoint_size
 
     end = time.time()
 
     re_found_hits = list(set(re_found_hits))
-    new_hists = list(set(new_hists))
+    new_hits = list(set(new_hits))
 
     print(f"Time taken: {(end - start)} seconds")
 
@@ -85,8 +101,8 @@ def search_for_known_hashes(
         print(
             f"Warning: Only re-found {len(re_found_hits)} of {len(known_track_names)} known track names!"
         )
-        print(f"Missing:")
+        print("Missing:")
         print(*sorted(set(known_track_names) - set(re_found_hits)), sep=", ")
 
-    print(f"Found {len(new_hists)} new hits:")
-    print(*sorted(new_hists), sep=", ")
+    print(f"Found {len(new_hits)} new hits:")
+    print(*sorted(new_hits), sep=", ")
